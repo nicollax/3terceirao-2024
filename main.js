6bb354ec4058bbcf0f5310a5ea43d4b46466ce60
nicollax <106092520+nicollax@users.noreply.github.com>
2024-12-05 11:01:10 -0300

    05/12

---
+++ main.js
@@ -0,0 +1,46 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+ for (let i = 0; i<botoes.length; i++) {
+    botoes [i].onclick = function(){
+       for (let j=0; j<botoes.length; j++){
+         botoes[j].classList.remove("ativo");
+         textos[j].classList.remove("ativo");
+}
+ botoes[i].classList.add("ativo");
+ textos[i].classList.add("ativo");
+   }
+}
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-28T23:59:59")
+
+const tempos = [
+   tempoObjetivo1
+]
+
+for (let i=0; i< contadores.length; i++){
+   contadores[i].textContent = calculaTempo(tempos[i]);
+}
+   contadores[0].textContent = calculaTempo(tempoObjetivo1)
+
+   function calculaTempo(tempoObjetivo1){
+      let tempoAtual = new Date();
+      let tempoFinal = tempoObjetivo1 - tempoAtual;
+      let segundos = Math.floor(tempoFinal / 1000);
+      let minutos = Math.floor(segundos / 60);
+      let horas = Math.floor(minutos / 60);
+      let dias = Math.floor(horas / 60);
+
+      segundos %= 60;
+      minutos %= 60;
+      horas %= 24;
+   
+      return dias + "dias"
+         + horas + "horas"
+         + minutos + "minutos"
+         + horas + "segundos"
+}
+
+
+
+
